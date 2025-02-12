c2be5f6dfeb1e5947dc98a033051739dccdfa844
Joe Hsiao <joe.phsiao@gmail.com>
2025-02-12 15:11:04 -0800

    complete 4.2

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const blogsRouter = require('./controllers/blogs')
 const cors = require('cors')
 const config = require('./utils/config')
 const mongoose = require('mongoose')
-
+const logger = require('./utils/logger')
 const app = express()
 
 app.use(cors())
@@ -12,5 +12,11 @@ app.use('/api/blogs', blogsRouter)
 
 const mongoUrl = config.MONGODB_URI
 mongoose.connect(mongoUrl)
+  .then(() => {
+    logger.info('Connected to MongoDB')
+  })
+  .catch(error => {
+    logger.error('Error connecting to MongoDB', error.message)
+  })
 
 module.exports = app
