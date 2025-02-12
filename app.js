2d3e13e31be27ffabfabafd205f47bb9b9b125b4
Joe Hsiao <joe.phsiao@gmail.com>
2025-02-12 15:01:44 -0800

    4.2 - move db connection to app.js

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 const express = require('express')
 const blogsRouter = require('./controllers/blogs')
 const cors = require('cors')
+const config = require('./utils/config')
+const mongoose = require('mongoose')
 
 const app = express()
 
 app.use(cors())
 app.use(express.json())
 app.use('/api/blogs', blogsRouter)
+
+const mongoUrl = config.MONGODB_URI
+mongoose.connect(mongoUrl)
 
 module.exports = app
--- models/blog.js
+++ models/blog.js
@@ -1,4 +1,3 @@
-const config = require('../utils/config')
 const mongoose = require('mongoose')
 
 const blogSchema = new mongoose.Schema({
@@ -8,7 +7,12 @@ const blogSchema = new mongoose.Schema({
   likes: Number
 })
 
-const mongoUrl = config.MONGODB_URI
-mongoose.connect(mongoUrl)
+blogSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
 
 module.exports = mongoose.model('Blog', blogSchema)
