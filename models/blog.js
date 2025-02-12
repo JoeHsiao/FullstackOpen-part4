3183a8e29b057a57ec491c3bea2ceedda39023ae
Joe Hsiao <joe.phsiao@gmail.com>
2025-02-12 14:08:04 -0800

    4.2 - create model

---
+++ models/blog.js
@@ -0,0 +1,14 @@
+require('dotenv').config()
+const mongoose = require('mongoose')
+
+const blogSchema = new mongoose.Schema({
+    title: String,
+    author: String,
+    url: String,
+    likes: Number
+})
+
+const mongoUrl = process.env.MONGODB_URI
+mongoose.connect(mongoUrl)
+
+module.exports = mongoose.model('Blog', blogSchema)
