0c5f68cea87e1eb4968de231bf00c1e05e0c4b0a
Joe Hsiao <joe.phsiao@gmail.com>
2025-02-12 17:36:01 -0800

    complete 4.4, skip 4.5, 4.6, 4.7

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,6 +2,12 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  return blogs.reduce((sum, b) => {
+    return sum + b.likes
+  }, 0)
+}
+
 module.exports = {
-  dummy
+  dummy, totalLikes
 }
