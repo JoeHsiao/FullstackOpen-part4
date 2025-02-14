9d1d25921b04f7c5f81c4023b72a472a4ec0f944
Joe Hsiao <joe.phsiao@gmail.com>
2025-02-14 09:35:17 -0800

    complete 4.18

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 const cors = require('cors')
 const config = require('./utils/config')
 const mongoose = require('mongoose')
@@ -11,6 +12,7 @@ app.use(cors())
 app.use(express.json())
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
 const mongoUrl = config.MONGODB_URI
 mongoose.connect(mongoUrl)
